76e0e215a1217d3da9910f7da7d4aa781e5f6086
Maciej Trybilo <maciektr@gmail.com>
2016-02-29 22:29:08 +0100

    Initial Commit

---
+++ BioSwift/BioSwift.h
@@ -0,0 +1,19 @@
+//
+//  BioSwift.h
+//  BioSwift
+//
+//  Created by Maciej Trybilo on 29/02/2016.
+//  Copyright © 2016 Maciej Trybilo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BioSwift.
+FOUNDATION_EXPORT double BioSwiftVersionNumber;
+
+//! Project version string for BioSwift.
+FOUNDATION_EXPORT const unsigned char BioSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BioSwift/PublicHeader.h>
+
+
