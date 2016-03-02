f886355c55cc5531534c35b8787992d242dcf0cd
Maciej Trybilo <maciektr@gmail.com>
2016-03-02 14:24:00 +0100

    FASTA reader with tests

---
+++ BioSwiftTests/FASTAReaderTests.swift
@@ -0,0 +1,20 @@
+//
+//  FASTAReaderTests.swift
+//  BioSwift
+//
+//  Created by Maciej Trybilo on 01/03/2016.
+//  Copyright © 2016 Maciej Trybilo. All rights reserved.
+//
+
+import XCTest
+
+class FASTAReaderTests: XCTestCase {
+
+    func test_readFromFile() {
+        
+        let sequences = FASTAReader.readFromFile(NSBundle.mainBundle().resourcePath! + "/example.fa")
+        
+        XCTAssertEqual(sequences.count, 3)
+    }
+
+}
